75b12416f70b99bf4dbaf801c3aeb0f7fb4d7c9d
Sam Crank <scrank@hex.tech>
2026-01-29 12:28:01 -0500

    update

--- src/Server.ts
+++ src/Server.ts
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Movie } from "./Types";
 import { MovieData } from "./MovieData";
 
@@ -11,23 +11,20 @@ export interface GetMoviesResult {
 export type TestState = "loading" | "error" | "empty";
 
 // loads movie data when the component mounts
-// reload() may be used to reload data at any time
 // optional testState can be used for testing
 export function useGetMovies(testState?: TestState): GetMoviesResult {
   const [data, setData] = useState<Movie[] | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  const loadData = () => {
+  useEffect(() => {
     setLoading(true);
     setTimeout(() => {
       setError(testState === "error");
       setData(testState === "empty" ? [] : MovieData);
       if (testState !== "loading") setLoading(false);
     }, 500);
-  };
-
-  if (data === null && !loading) loadData();
+  }, []);
 
   return { data, loading, error };
 }
